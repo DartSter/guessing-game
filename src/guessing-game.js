4257bb4b927e9a2420344a15342086a484abce71
DartSter <kil-all@rambler.ru>
2023-02-27 11:35:25 +0200

    'init'

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max
+        this.min
+        this.middle 
 
+    }
+
+    findMiddle() {
+        this.middle = Math.round((this.max-this.min)/2)+this.min
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.findMiddle()
     }
 
     guess() {
-
+        return this.middle
     }
 
     lower() {
-
+        this.max = this.middle
+        this.findMiddle()
     }
 
     greater() {
-
+        this.min = this.middle
+        this.findMiddle()
     }
 }
 
